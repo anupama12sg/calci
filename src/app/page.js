02453efa06778566c7acc01f37daba7fc6d83c23
Anupama Singh <anupama12sg@gmail.com>
2024-08-18 14:13:54 +0530

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,19 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
-    </main>
+    <div className="container">
+      <div className="calculator">
+        <form action="">
+          <div>
+            <input type="text" />
+
+
+          </div>
+        </form>
+      </div>
+
+
+    </div>
+
   );
 }
