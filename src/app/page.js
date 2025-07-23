2727cd5363a53eb347c427bd1ddfc8fc2fa2da9b
Aditya <anupama12sg@gmail.com>
2025-07-23 22:46:47 +0530

    chore: minor formatting

--- src/app/page.js
+++ src/app/page.js
@@ -17,48 +17,50 @@ export default function Home() {
         setInput("Error");
       }
     } else {
-      setInput(input + value); 
+      setInput(input + value);
     }
   };
 
   return (
-    <><h1><center>MY CALCULATOR</center></h1><div className="container">
-      <div className="calculator">
-        <form action="">
-          <div className="display">
-            <input type="text" value={input} readOnly />
-          </div>
-          <div className="buttons">
-            <input type="button" value="AC" onClick={() => handleClick("AC")} />
-            <input type="button" value="DE" onClick={() => handleClick("DE")} />
-            <input type="button" value="." onClick={() => handleClick(".")} />
-            <input type="button" value="/" onClick={() => handleClick("/")} />
-          </div>
-          <div>
-            <input type="button" value="7" onClick={() => handleClick("7")} />
-            <input type="button" value="8" onClick={() => handleClick("8")} />
-            <input type="button" value="9" onClick={() => handleClick("9")} />
-            <input type="button" value="*" onClick={() => handleClick("*")} />
-          </div>
-          <div>
-            <input type="button" value="4" onClick={() => handleClick("4")} />
-            <input type="button" value="5" onClick={() => handleClick("5")} />
-            <input type="button" value="6" onClick={() => handleClick("6")} />
-            <input type="button" value="+" onClick={() => handleClick("+")} />
-          </div>
-          <div>
-            <input type="button" value="1" onClick={() => handleClick("1")} />
-            <input type="button" value="2" onClick={() => handleClick("2")} />
-            <input type="button" value="3" onClick={() => handleClick("3")} />
-            <input type="button" value="-" onClick={() => handleClick("-")} />
-          </div>
-          <div>
-            <input type="button" value="00" onClick={() => handleClick("00")} />
-            <input type="button" value="0" onClick={() => handleClick("0")} />
-            <input type="button" value="=" onClick={() => handleClick("=")} className="equal" />
-          </div>
-        </form>
-      </div>
-    </div></>
+    <><title style={{ backgroundColor: "transparent", color: "white" }}>
+    </title>
+      <div className="container">
+        <div className="calculator">
+          <form action="">
+            <div className="display">
+              <input type="text" value={input} readOnly />
+            </div>
+            <div className="buttons">
+              <input type="button" value="AC" onClick={() => handleClick("AC")} />
+              <input type="button" value="DE" onClick={() => handleClick("DE")} />
+              <input type="button" value="." onClick={() => handleClick(".")} />
+              <input type="button" value="/" onClick={() => handleClick("/")} />
+            </div>
+            <div>
+              <input type="button" value="7" onClick={() => handleClick("7")} />
+              <input type="button" value="8" onClick={() => handleClick("8")} />
+              <input type="button" value="9" onClick={() => handleClick("9")} />
+              <input type="button" value="*" onClick={() => handleClick("*")} />
+            </div>
+            <div>
+              <input type="button" value="4" onClick={() => handleClick("4")} />
+              <input type="button" value="5" onClick={() => handleClick("5")} />
+              <input type="button" value="6" onClick={() => handleClick("6")} />
+              <input type="button" value="+" onClick={() => handleClick("+")} />
+            </div>
+            <div>
+              <input type="button" value="1" onClick={() => handleClick("1")} />
+              <input type="button" value="2" onClick={() => handleClick("2")} />
+              <input type="button" value="3" onClick={() => handleClick("3")} />
+              <input type="button" value="-" onClick={() => handleClick("-")} />
+            </div>
+            <div>
+              <input type="button" value="00" onClick={() => handleClick("00")} />
+              <input type="button" value="0" onClick={() => handleClick("0")} />
+              <input type="button" value="=" onClick={() => handleClick("=")} className="equal" />
+            </div>
+          </form>
+        </div>
+      </div></>
   );
 }
